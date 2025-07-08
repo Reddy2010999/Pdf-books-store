c155b271883729ba03ae2cfd097642a27aa9737b
Reddy2010999 <jangaparikshithreddy@gmail.com>
2025-07-08 22:40:52 +0530

    deleted firebase-config.js (moved config into script.js)

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyD5eabtRXkVrFZqVd4tudD9cwQ6cibuWyE",
-  authDomain: "parikshith-reddy-books-store.firebaseapp.com",
-  projectId: "parikshith-reddy-books-store",
-  storageBucket: "parikshith-reddy-books-store.appspot.com",
-  messagingSenderId: "102247596219",
-  appId: "1:102247596219:web:a9ad13f87f4fe65b78e99e"
-};
